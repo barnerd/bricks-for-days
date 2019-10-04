41c038d6705a169f2084eaaeb4de6741276d361d
Verage <dibarner@gmail.com>
2019-10-04 16:44:08 -0700

    Added Extra Live PowerUp Gain event

--- Assets/Scripts/PowerUps/PowerUpExtraLives.cs
+++ Assets/Scripts/PowerUps/PowerUpExtraLives.cs
@@ -7,11 +7,14 @@ public class PowerUpExtraLives : PowerUp
     public int extraLives = 1;
     public IntVariable playerLives;
 
+    public GameEvent OnPowerUpExtraLivesGain;
+
     public override void UsePowerUpPayload()
     {
         base.UsePowerUpPayload();
 
         // Payload is to add lives
         playerLives.Value += extraLives;
+        OnPowerUpExtraLivesGain.Raise();
     }
 }
