0f164aebdcaf961259fa58588e19f534c189fdf0
Verage <dibarner@gmail.com>
2019-10-18 16:01:36 -0700

    Add max lives gained by power up

--- Assets/Scripts/PowerUps/PowerUpAddInt.cs
+++ Assets/Scripts/PowerUps/PowerUpAddInt.cs
@@ -5,11 +5,13 @@ using System.Collections;
 public class PowerUpAddInt : PowerUp
 {
     public int _value;
+    public int minValue;
+    public int maxValue;
     public IntReference _intSetting;
 
     public override void UsePowerUpPayload()
     {
-        _intSetting.Value += _value;
+        _intSetting.Value = Mathf.Clamp(_intSetting.Value + _value, minValue, maxValue);
 
         base.UsePowerUpPayload();
     }
