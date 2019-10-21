93d7146a971fe339988495355a4523652c3c650b
Verage <dibarner@gmail.com>
2019-10-20 18:55:24 -0700

    Modify Wall Edges by script
    Moved walls to UI. Changed wall colliders to edge colliders and moved setting them to script, to handle multiple screen resolutions.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,6 +14,7 @@ public class GameManager : MonoBehaviour
     public float AITimeScale;
     public KeyCode pauseKey;
     public GameEvent OnLevelStart;
+    private Camera camera;
 
     [Header("UI Components")]
     public GameObject pauseUI;
@@ -35,28 +36,71 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
+        camera = Camera.main;
+        GenerateCollidersAcrossScreen();
+
         Time.timeScale = AITimeScale;
 
-        // Add function call to wantsToQuit event
-        Application.wantsToQuit += Exiting;
-        hs = new Highscores();
-        hs.Load();
+        if (highscoresUI != null)
+        {
+            // Add function call to wantsToQuit event
+            Application.wantsToQuit += Exiting;
+            hs = new Highscores();
+            hs.Load();
 
-        // Update UI with highscores
-        UpdateHighScoresUI();
+            // Update UI with highscores
+            UpdateHighScoresUI();
+        }
     }
 
     private void Update()
     {
         if (Input.GetKeyUp(pauseKey))
         {
-            if(pauseUI != null)
+            if (pauseUI != null)
             {
                 pauseUI.GetComponent<ScreenController>().SetWindowState(!Mathf.Approximately(Time.timeScale, 0f));
             }
 
             Time.timeScale = (!Mathf.Approximately(Time.timeScale, 0f)) ? 0f : 1f;
         }
+    }
+
+    void GenerateCollidersAcrossScreen()
+    {
+        Vector2 bottomLeftCorner = camera.ViewportToWorldPoint(new Vector3(0, 0f, camera.nearClipPlane));
+        Vector2 topRightCorner = camera.ViewportToWorldPoint(new Vector3(1f, 1f, camera.nearClipPlane));
+        float height = topRightCorner.y - bottomLeftCorner.y;
+        float width = topRightCorner.x - bottomLeftCorner.x;
+        Vector2[] colliderPoints;
+
+        EdgeCollider2D upperEdge = transform.Find("topWall").GetComponent<EdgeCollider2D>();
+        colliderPoints = upperEdge.points;
+        colliderPoints[0] = new Vector2(bottomLeftCorner.x, 0);
+        colliderPoints[1] = new Vector2(topRightCorner.x, 0);
+        upperEdge.points = colliderPoints;
+        upperEdge.offset = new Vector2(0, topRightCorner.y - height * .08f); // move topWall 8% below top of screen
+
+        EdgeCollider2D lowerEdge = transform.Find("bottomWall").GetComponent<EdgeCollider2D>();
+        colliderPoints = lowerEdge.points;
+        colliderPoints[0] = new Vector2(bottomLeftCorner.x, 0);
+        colliderPoints[1] = new Vector2(topRightCorner.x, 0);
+        lowerEdge.points = colliderPoints;
+        lowerEdge.offset = new Vector2(0, bottomLeftCorner.y - height * .1f); // move bottomWall 10% below screen
+
+        EdgeCollider2D leftEdge = transform.Find("leftWall").GetComponent<EdgeCollider2D>();
+        colliderPoints = leftEdge.points;
+        colliderPoints[0] = new Vector2(0, topRightCorner.y);
+        colliderPoints[1] = new Vector2(0, bottomLeftCorner.y);
+        leftEdge.points = colliderPoints;
+        leftEdge.offset = new Vector2(bottomLeftCorner.x + width * .09f, 0); // move leftWall in 9%
+
+        EdgeCollider2D rightEdge = transform.Find("rightWall").GetComponent<EdgeCollider2D>();
+        colliderPoints = rightEdge.points;
+        colliderPoints[0] = new Vector2(0, topRightCorner.y);
+        colliderPoints[1] = new Vector2(0, bottomLeftCorner.y);
+        rightEdge.points = colliderPoints;
+        rightEdge.offset = new Vector2(topRightCorner.x - width * .09f, 0); // move rightWall in 9%
     }
 
     public void StartGame()
@@ -71,7 +115,7 @@ public class GameManager : MonoBehaviour
     // TODO: move to UI Manager?
     public void LevelWon()
     {
-        if(levelCompleteUI != null)
+        if (levelCompleteUI != null)
         {
             levelCompleteUI.GetComponent<ScreenController>().StartNotificationFade();
         }
